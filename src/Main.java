0899cf20f2836fa389b767e3db1a42b49b0b1467
Evgeniy Pusser <evgeniipusser88@gmail.com>
2024-03-16 00:17:34 +0300

    Joke

--- src/Main.java
+++ src/Main.java
@@ -1,17 +1,25 @@
+import java.util.Arrays;
+
 // Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
 // then press Enter. You can now see whitespace characters in your code.
 public class Main {
     public static void main(String[] args) {
-        // Press Alt+Enter with your caret at the highlighted text to see how
-        // IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
-
-        // Press Shift+F10 or click the green arrow button in the gutter to run the code.
-        for (int i = 1; i <= 5; i++) {
-
-            // Press Shift+F9 to start debugging your code. We have set one breakpoint
-            // for you, but you can always add more by pressing Ctrl+F8.
-            System.out.println("i = " + i);
-        }
+        int[] numbers = new int []{1,67,4,5,66,0,23,4,6,23,4,77,4};
+           System.out.println(Arrays.toString((changeArray(numbers, 23))));
+           }
+           public static int[]changeArray(int [] myArray, int number){
+        int position = myArray.length - 1;
+        for(int i = 0; i < position; i++){
+            if(myArray[i] == number){
+                while(myArray[position] == number && position > i){
+                    position--;
+                }int temp = myArray[i];
+                myArray[i]= myArray[position];
+                myArray[position]= temp;
+                position--;
+            }
+        }return myArray;
     }
+
+
 }
